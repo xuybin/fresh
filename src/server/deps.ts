b02f4cc9c14a31ceea3c2bfa5b14e86666b87ad4
xuybin <xuybin@qq.com>
2022-03-30 11:08:34 +0800

    refactor:

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -6,13 +6,13 @@ export {
   extname,
   fromFileUrl,
   toFileUrl,
-} from "https://deno.land/std@0.130.0/path/mod.ts";
-export { walk } from "https://deno.land/std@0.130.0/fs/walk.ts";
-export { serve } from "https://deno.land/std@0.130.0/http/server.ts";
+} from "https://deno.land/std/path/mod.ts";
+export { walk } from "https://deno.land/std/fs/walk.ts";
+export { serve } from "https://deno.land/std/http/server.ts";
 export type {
   ConnInfo,
   Handler as RequestHandler,
-} from "https://deno.land/std@0.130.0/http/server.ts";
+} from "https://deno.land/std/http/server.ts";
 
 // -- router --
 export * as router from "./router.ts";
@@ -23,11 +23,11 @@ export { lookup as mediaTypeLookup } from "https://deno.land/x/media_types@v2.12
 // -- esbuild --
 // @deno-types="https://deno.land/x/esbuildx@v0.14.26/mod.d.ts"
 import * as esbuildWasm from "./esbuild-wasm/browser.js";
-import * as esbuildNative from "https://deno.land/x/esbuildx@v0.14.26/mod.js";
+import * as esbuildNative from "https://deno.land/x/esbuildx/mod.js";
 
 // @ts-ignore trust me
 const esbuild: typeof esbuildWasm = Deno.run === undefined
   ? esbuildWasm
   : esbuildNative;
 export { esbuild, esbuildWasm as esbuildTypes };
-export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.4.1/mod.ts";
+export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader/mod.ts";
