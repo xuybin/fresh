f655a5718e90a73bea27f1b82914f3c972f75f9e
xuybin <xuybin@qq.com>
2022-04-08 20:58:19 +0800

    refactor:

--- src/cli/start.ts
+++ src/cli/start.ts
@@ -5,9 +5,10 @@
 /// <reference lib="deno.unstable" />
 /// <reference types="../../types.d.ts" />
 
-import { start,Manifest } from "../server/mod.ts";
+import { Manifest, start } from "../server/mod.ts";
 import { error } from "./error.ts";
-import {resolve,toFileUrl} from "https://deno.land/std/path/mod.ts";
+import { resolve, toFileUrl } from "https://deno.land/std/path/mod.ts";
+import { parseArgs } from "./deps.ts";
 
 const help = `fresh start
 
@@ -26,42 +27,59 @@ OPTIONS:
     -h, --help                 Prints help information
 `;
 export interface Args {
-    help: boolean;
+  help: boolean;
 }
 
 export async function startSubcommand(rawArgs: Record<string, any>) {
-    const args: Args = {
-      help: !!rawArgs.help,
-    };
-    const directory: string | null = typeof rawArgs._[0] === "string"
-      ? rawArgs._[0]
-      : null;
-    if (args.help) {
-      console.log(help);
-      Deno.exit(0);
-    }
-    if (directory === null) {
-        console.error(help);
-        error("No route file given.");
-        Deno.exit(0);
-    }
-    let importUrl=directory;
-    if(!directory.toLowerCase().startsWith("http")){
-      try {
-        importUrl=resolve(Deno.cwd(),directory)
-        //console.log(importUrl)
-        const fileInfo = await Deno.stat(importUrl);
-        if(!fileInfo.isFile){
-          error("given route file not isFile.");
-        }
-        importUrl=toFileUrl(importUrl).href
-        //console.log(importUrl)
-      } catch (err) {
+  const args: Args = {
+    help: !!rawArgs.help,
+  };
+  const directory: string | null = typeof rawArgs._[0] === "string"
+    ? rawArgs._[0]
+    : null;
+  if (args.help) {
+    console.log(help);
+    Deno.exit(0);
+  }
+  if (directory === null) {
+    //console.error(help);
+    error("No route file given.");
+  }
+  let importUrl = directory;
+  if (!directory.toLowerCase().startsWith("http")) {
+    try {
+      importUrl = resolve(Deno.cwd(), directory);
+      //console.log(importUrl)
+      const fileInfo = await Deno.stat(importUrl);
+      if (!fileInfo.isFile) {
         error("given route file not isFile.");
       }
+      importUrl = toFileUrl(importUrl).href;
+      //console.log(importUrl)
+    } catch (err) {
+      error("given route file not isFile.");
     }
-    const manifest=await import(importUrl);
-    //console.error(JSON.stringify(manifest.default));
-    await start(manifest.default as Manifest);
   }
-  
+  const manifest = await import(importUrl);
+  //console.error(JSON.stringify(manifest.default));
+  await start(manifest.default as Manifest);
+}
+
+if (import.meta.main) {
+  const args = parseArgs(Deno.args, {
+    alias: {
+      "help": "h",
+      "version": "V",
+    },
+    boolean: [
+      "help",
+      "version",
+    ],
+  });
+  if (args.version) {
+    console.log(`${import.meta.url}`);
+    Deno.exit(0);
+  }else{
+    await startSubcommand(args);
+  }
+}
